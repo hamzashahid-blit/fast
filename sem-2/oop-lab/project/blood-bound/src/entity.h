08012a05a52520d9959b98f1bad0622e322e7a69
Your Name <you@example.com>
2025-05-28 20:46:47 +0500

    Working Single Player + ImGui

--- sem-2/oop-lab/project/blood-bound/src/entity.h
+++ sem-2/oop-lab/project/blood-bound/src/entity.h
@@ -1,50 +1,177 @@
 #pragma once
+#include "glm/detail/qualifier.hpp"
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <string>
+#include <map>
+#include <vector>
+#include <sys/types.h>
+#include <iostream>
 
-class Entity {
-    glm::vec2 m_position;
-    std::string m_tex_path; // TODO: turn to spritesheet
+#define ASPECT_RATIO (16.0f/9.0f) //(static_cast<float>(WIN_WIDTH) / WIN_HEIGHT)
+#define TILE_SCALE 0.3f
+#define PLAYER_SCALE 0.3f
+#define PLAYER_SCALE_WIDTH 0.75f
+#define TILE_HEIGHT (TILE_SCALE * 0.5f)
+#define TILE_WIDTH (2 / (ASPECT_RATIO - -ASPECT_RATIO) * TILE_SCALE * 0.5f)
+#define PLAYER_WIDTH (TILE_WIDTH * PLAYER_SCALE_WIDTH)
+#define PLAYER_HEIGHT TILE_HEIGHT
+#define GRAVITY 0.0005f //9.81f
+#define JUMP_FACTOR -0.015f
+
+// SS = Spritesheet
+template <typename Type>
+class SpriteSheet {
+    std::string m_path;
+    glm::vec2 m_tile_dimensions;
+    glm::vec2 m_file_dimensions;
+    glm::vec2 m_tile_padding;
     unsigned int m_tex;
+    int m_channel_count;
+    std::map<Type, glm::vec2> m_map;
 public:
-    Entity(float x, float y, std::string_view tex_path) : m_position(x, y), m_tex_path(tex_path) {
-        int tex_width, tex_height, tex_channel_count;
-        unsigned char *tex_data = stbi_load(m_tex_path.c_str(), &tex_width, &tex_height, &tex_channel_count, 0);
+  SpriteSheet(std::string_view path, glm::vec2 tile_dimensions,
+              glm::vec2 tile_padding, std::map<Type, glm::vec2> map)
+      : m_path(path), m_tile_dimensions(tile_dimensions),
+        m_tile_padding(tile_padding), m_map(map) {
+        int width, height;
+        unsigned char *tex_data = stbi_load(m_path.c_str(), &width, &height, &m_channel_count, 0);
+        m_file_dimensions = {width, height}; // TODO: inline
         glGenTextures(1, &m_tex);
         glBindTexture(GL_TEXTURE_2D, m_tex);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_width, tex_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_file_dimensions.x, m_file_dimensions.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_data);
         glGenerateMipmap(GL_TEXTURE_2D);
         stbi_image_free(tex_data);
     }
 
-    glm::vec2 get_position() const { return m_position; }    
-    std::string get_tex_path() const { return m_tex_path; }
-    unsigned int get_tex() const { return m_tex; }    
+    SpriteSheet(std::string_view path, glm::vec2 tile_dimensions, glm::vec2 tile_padding)
+        : SpriteSheet(path, tile_dimensions, tile_padding, {}) {}
+
+    void bind() const { glBindTexture(GL_TEXTURE_2D, m_tex); }
+
+    std::string get_path() const { return  m_path; };
+    glm::vec2 get_tile_dimensions() const { return m_tile_dimensions; };
+    glm::vec2 get_file_dimensions() const { return m_file_dimensions; };
+    glm::vec2 get_tile_padding() const { return  m_tile_padding; };
+    int get_channel_count() const { return  m_channel_count; };
+    unsigned int get_tex() const { return m_tex; };
+    std::map<Type, glm::vec2> get_map() const { return m_map; };
+
+    void set_map(std::map<Type, glm::vec2> val) { m_map = val; };
+};
+
+class Entity {
+protected:
+    glm::vec2 m_position;
+public:
+    Entity(float x, float y) : m_position(x, y) {}
+
+    glm::vec2 get_position() const { return m_position; }
+    void set_position(glm::vec2 val) { m_position = val; }
+    void add_position(glm::vec2 val) { m_position += val; }
+};
+
+// m = mid, r = right, l = left
+enum class TileType {
+    grass_m,
+    grass_l,
+    grass_r,
+    dirt_m,
+    none,
+};
+
+const std::vector<std::vector<TileType>> LEVEL_1 = {
+    {},
+    {},
+    {},
+    {TileType::none, TileType::none, TileType::none, TileType::none,
+     TileType::none, TileType::grass_m},
+    {TileType::grass_l, TileType::grass_r, TileType::none, TileType::grass_l,
+     TileType::grass_m, TileType::grass_m, TileType::grass_r},
+};
+class Level {
+    std::vector<std::vector<TileType>> data;
+public:
+};
+
+const std::map<TileType, glm::vec2> TILE_MAP {
+    {TileType::grass_m, {7, 8}},
+    {TileType::grass_l, {7, 9}},
+    {TileType::grass_r, {7, 7}},
+    {TileType::dirt_m, {10, 12}}, // 8,12
+    {TileType::none, {11, 12}},
 };
 
 class Tile : public Entity {
+    TileType m_type;
 public:
-    Tile(float x, float y, std::string_view tex_path) : Entity(x, y, tex_path) {}
+    Tile(float x, float y, TileType type)
+        : Entity(x, y), m_type(type) {}
+
+    TileType get_type() { return m_type; };
 };
 
 class Character : public Entity {
+protected:
     glm::vec2 m_velocity;
 public:
-    Character(float x, float y, std::string_view tex_path)
-        : Entity(x, y, tex_path), m_velocity(0.0f, 0.0f) {}
-    Character(float x, float y, std::string_view tex_path, float vel_x, float vel_y)
-        : Entity(x, y, tex_path), m_velocity(vel_x, vel_y) {}
+    Character(float x, float y)
+        : Entity(x, y), m_velocity(0.0f, 0.0f) {}
+    Character(float x, float y, float vel_x, float vel_y)
+        : Entity(x, y), m_velocity(vel_x, vel_y) {}
 
     glm::vec2 get_velocity() const { return m_velocity; }
+    void set_velocity(glm::vec2 val) { m_velocity = val; }
+    void add_velocity(glm::vec2 val) { m_velocity += val; }
+};
+
+enum class PlayerFrame {
+    stand
+};
+
+const std::map<PlayerFrame, glm::vec2> PLAYER_MAP {
+    {PlayerFrame::stand, {0, 0}},
 };
 
 class Player : public Character {
+    PlayerFrame m_current_frame = PlayerFrame::stand;
+    bool m_is_falling = false;
+    glm::vec2 m_spawnpoint;
 public:
-    Player(float x, float y, std::string_view tex_path)
-        : Character(x, y, tex_path) {}
+    Player(glm::vec2 spawnpoint)
+        : Character(spawnpoint.x, spawnpoint.y), m_spawnpoint(spawnpoint) {}
+
+    Player() : Player({TILE_WIDTH * 2, TILE_HEIGHT * 2}) {}
+
+    void respawn() {
+        this->set_velocity({0.0f, 0.0f});
+        this->set_is_falling(false);
+        this->set_position(m_spawnpoint);
+    }
+    
+    PlayerFrame get_current_frame() { return m_current_frame; }
+    bool get_is_falling() { return m_is_falling; }
+    void set_is_falling(bool val) { m_is_falling = val; }
+};
+
+class CollisionRect {
+    float m_x, m_y, m_w, m_h;
+    Entity *m_actor;
+    Entity *m_entity;
+public:
+    CollisionRect(float x, float y, float w, float h, Entity* actor, Entity* entity) 
+        : m_x(x), m_y(y), m_w(w), m_h(h), m_actor(actor), m_entity(entity) {}
+
+    float get_area() const { return m_w * m_h; }
+    
+    float get_x() const { return m_x; }
+    float get_y() const { return m_y; }
+    float get_w() const { return m_w; }
+    float get_h() const { return m_h; }
+    Entity* get_actor() const { return m_actor; }
+    Entity* get_entity() const { return m_entity; }
 };
--- sem-2/oop-lab/project/blood-bound/src/main.cpp
+++ sem-2/oop-lab/project/blood-bound/src/main.cpp
@@ -1,19 +1,25 @@
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <ostream>
+#include <array>
 #include <vector>
+#include <algorithm>
+#include <climits>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
+#include <glm/fwd.hpp>
 
 #include "entity.h"
 
 #define INFO_LOG_LEN 512
-#define ASPECT_RATIO (static_cast<float>(WIN_WIDTH) / WIN_HEIGHT)
+#define AA_BORDER 0.001f
 
 constexpr int WIN_FACTOR = 100;
 int WIN_WIDTH = 16 * WIN_FACTOR;
@@ -55,8 +61,16 @@ const char *fragment_shader_source =
     "in vec2 fTexCoord;\n"
     "out vec4 oColor;\n"
     "uniform sampler2D Tex;"
+    "uniform vec2 uTILE_SS_COUNT;\n" // ss = spritesheet; amount of tiles
+    "uniform vec2 uTILE_SS_INDEX;\n" // ss = spritesheet; index of tile, in
+                                     // terms of tiles
+    "uniform vec2 uTILE_SS_PADDING;\n" // floats padding_pixels_per_tile/ss_width_in_pixels
+    "vec2 TILE_SIZE;\n"
+    "vec2 texCoord;\n"
     "void main() {\n"
-    "    oColor = texture(Tex, fTexCoord) * vec4(1.0, 1.0, 1.0, 1.0);\n"
+    "    TILE_SIZE = 1.0/uTILE_SS_COUNT;\n"
+    "    texCoord = TILE_SIZE * uTILE_SS_INDEX + fTexCoord*(TILE_SIZE - uTILE_SS_PADDING);\n"
+    "    oColor = texture(Tex, texCoord) * vec4(1.0, 1.0, 1.0, 1.0);\n"
     "}\0";
 
 int shader_success;
@@ -69,10 +83,12 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 }    
 
 int main() {
+#if 0
+#else
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT,
+    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT,                                          
                                           "Blood Bound", nullptr, nullptr);
     if (window == nullptr) {
         std::cerr << "[ERROR] Failed to create a GLFW Window!" << std::endl;
@@ -80,11 +96,34 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetWindowAspectRatio(window, 16, 9);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
+
     if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
         std::cerr << "[ERROR] Failed to initialize GLAD!" << std::endl;
         return -1;
     }
 
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 400");
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+    auto tile_ss = SpriteSheet<TileType>("res/Base pack/Tiles/tiles_spritesheet.png", 
+                                         {12, 13}, {2, 2}, TILE_MAP);
+    auto player_1_ss = SpriteSheet<PlayerFrame>("res/Base pack/Player/p1_spritesheet.png",
+                                                {7, 3}, {0, 0}, PLAYER_MAP);
+    
+    // std::cout << tile_ss.get_tile_dimensions().x << ' ' << tile_ss.get_tile_dimensions().y
+    //           << ',' << tile_ss.get_tile_padding().x << ' ' << tile_ss.get_tile_padding().y 
+    //           << ',' << tile_ss.get_map()[TileType::dirt_m].x << ' ' << tile_ss.get_map()[TileType::dirt_m].y;
     unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
     glCompileShader(vertex_shader);
@@ -149,8 +188,23 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
 
-    auto grass_tile = Tile(0.5f, 0.5f, "res/Base pack/Tiles/grass.png");
+    unsigned int model_uniform_loc = glGetUniformLocation(shader_program, "uModel");
+    unsigned int view_uniform_loc = glGetUniformLocation(shader_program, "uView");
+    unsigned int projection_uniform_loc = glGetUniformLocation(shader_program, "uProjection");
+    unsigned int tile_ss_count_uniform_loc = glGetUniformLocation(shader_program, "uTILE_SS_COUNT");
+    unsigned int tile_ss_index_uniform_loc = glGetUniformLocation(shader_program, "uTILE_SS_INDEX");
+    unsigned int tile_ss_padding_uniform_loc = glGetUniformLocation(shader_program, "uTILE_SS_PADDING");
     
+    std::vector<Tile> tiles = {};
+    for (int i = 0; i < LEVEL_1.size(); i++) {
+        for (int j = 0; j < LEVEL_1[i].size(); j++) {
+            tiles.emplace_back(TILE_WIDTH*j+TILE_WIDTH/2-(AA_BORDER*j), 
+                               TILE_HEIGHT*i+TILE_HEIGHT/2-(AA_BORDER*i), LEVEL_1[i][j]);
+        }
+    }
+
+    auto player = Player({TILE_WIDTH * 2, TILE_HEIGHT * 2});
+
     glViewport(0, 0, WIN_WIDTH, WIN_HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glEnable(GL_BLEND);
@@ -158,44 +212,213 @@ int main() {
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 
-    unsigned int model_uniform_loc = glGetUniformLocation(shader_program, "uModel");
-    unsigned int view_uniform_loc = glGetUniformLocation(shader_program, "uView");
-    unsigned int projection_uniform_loc = glGetUniformLocation(shader_program, "uProjection");
+    static bool use_work_area = true;
+    static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings;
+
+    // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+    // Based on your use case you may want one or the other.
+    const ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+    ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
     
+    auto projection_mat =
+        glm::ortho(-ASPECT_RATIO, ASPECT_RATIO, -1.0f, 1.0f, 0.1f, 100.0f);
+
+    bool is_first_time = true;
+    glm::vec2 camera_pos = {0.0f, 0.0f};
+    std::array<bool, SHRT_MAX> keys_pressed = {false};
     while (!glfwWindowShouldClose(window)) {
-        /// Input
-        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-            std::cout << "Yay!\n";
-        /// Update Then Render
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(shader_program);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, grass_tile.get_tex());
-        glBindVertexArray(vao);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        
-        auto model_mat = glm::mat4(1.0f);
-        model_mat = glm::translate(model_mat,
-                                   glm::vec3(-ASPECT_RATIO + grass_tile.get_position().x * 2*ASPECT_RATIO,
-                                             1.0 - grass_tile.get_position().y * 2,
-                                             0.0f));
-        // model_mat = glm::scale(model_mat, glm::vec3(0.2f));
-        glUniformMatrix4fv(model_uniform_loc, 1, GL_FALSE, glm::value_ptr(model_mat));
-        auto view_mat = glm::mat4(1.0f);
-        view_mat = glm::translate(view_mat, glm::vec3(0.0f, 0.0f, -1.0f));
-        glUniformMatrix4fv(view_uniform_loc, 1, GL_FALSE, glm::value_ptr(view_mat));
-        auto projection_mat =
-            glm::ortho(-ASPECT_RATIO, ASPECT_RATIO, -1.0f, 1.0f, 0.1f, 100.0f);
-        glUniformMatrix4fv(projection_uniform_loc, 1, GL_FALSE, glm::value_ptr(projection_mat));
-        
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-        /// Swap & Poll
-        glfwSwapBuffers(window);
         glfwPollEvents();
-    }
+
+        if (is_first_time) {
+            if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
+                ImGui_ImplGlfw_Sleep(10);
+                continue;
+            }
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            {
+                ImGui::Begin("Blood Bound");
+                ImGui::Text("Blood Bound");
+                if (ImGui::Button("Play"))
+                    is_first_time = false;
+                ImGui::End();
+            }
+
+            // Rendering
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            glfwSwapBuffers(window);
+        } else {
     
+            /// Input
+            for (int key = 0; key < keys_pressed.size(); key++) {
+                keys_pressed[key] = (glfwGetKey(window, key) == GLFW_PRESS);
+            }
+        
+            if (keys_pressed[GLFW_KEY_D]) {
+                player.add_position(glm::vec2(0.003f, 0.0f));
+            } if (keys_pressed[GLFW_KEY_A]) {
+                player.add_position(glm::vec2(-0.003f, 0.0f));
+            } if (keys_pressed[GLFW_KEY_W]) {
+                if (!player.get_is_falling()) {
+                    player.add_velocity(glm::vec2(0.0f, JUMP_FACTOR));
+                    player.set_is_falling(true);
+                }
+            }
+            // Update
+            player.add_velocity(glm::vec2(0.0f, GRAVITY));
+            player.add_position(player.get_velocity());
+
+            if (player.get_position().y - PLAYER_HEIGHT / 2 > 1.0)
+                player.respawn();
+
+            std::vector<CollisionRect> coll_rects = {};
+            for (Tile entity : tiles) {
+                if (entity.get_type() == TileType::none) continue;
+                float pl = player.get_position().x - PLAYER_WIDTH/2;
+                float pr = player.get_position().x + PLAYER_WIDTH/2;
+                float pt = player.get_position().y - PLAYER_HEIGHT/2;
+                float pb = player.get_position().y + PLAYER_HEIGHT/2;
+                float el = entity.get_position().x - TILE_WIDTH/2;
+                float er = entity.get_position().x + TILE_WIDTH/2;
+                float et = entity.get_position().y - TILE_HEIGHT/2;
+                float eb = entity.get_position().y + TILE_HEIGHT/2;
+                if (pl < er && pr > el && pt < eb && pb > et) {  // collision
+                    float cx, cy, cw, ch;
+                    if (player.get_position().y <= entity.get_position().y) { // player on top or inline
+                        ch = std::abs(pb - et);
+                        cy = ch/2 + et;
+                    } else { // player on bottom
+                        ch = std::abs(pt - eb);
+                        cy = ch/2 + pt;
+                    }
+                    if (player.get_position().x >= entity.get_position().x) { // Player on right or inline
+                        cw = std::abs(pl - er);
+                        cx = cw/2 + pl;
+                    } else { // player on left 
+                        cw = std::abs(pr - el);
+                        cx = cw/2 + el;
+                    }
+                    coll_rects.emplace_back(cx, cy, cw, ch, &player, &entity);
+                }
+            }
+
+            auto largest_coll_rect_it = std::max_element(coll_rects.cbegin(), coll_rects.cend(), 
+                                                         [](CollisionRect r1, CollisionRect r2){
+                                                             return r1.get_area() < r2.get_area();
+                                                         });
+        
+            if (largest_coll_rect_it != coll_rects.end()) { // Collision detected
+                CollisionRect rect = *largest_coll_rect_it;
+                Character* r_actor = static_cast<Character*>(rect.get_actor());
+                Entity* r_entity = rect.get_entity();
+                // Find smallest axis
+                if (rect.get_w() < rect.get_h()) {
+                    if (r_actor->get_position().x >= r_entity->get_position().x) { // Player on right or inline
+                        r_actor->add_position(glm::vec2(rect.get_w(), 0.0f));
+                    } else { // player on left
+                        r_actor->add_position(glm::vec2(-rect.get_w(), 0.0f));
+                    }
+                } else {
+                    if (r_actor->get_position().y <= r_entity->get_position().y) { // player on top
+                        r_actor->set_velocity(glm::vec2(r_actor->get_velocity().x, 0.0f));
+                        r_actor->add_position(glm::vec2(0.0f, -rect.get_h()));
+                        player.set_is_falling(false);
+                    } else { // player on bottom
+                        r_actor->add_position(glm::vec2(0.0f, rect.get_h()));
+                    }
+                }
+            } else {
+                player.set_is_falling(true);
+            }
+        
+            /// Render
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glUseProgram(shader_program);
+            // glActiveTexture(GL_TEXTURE0);
+
+            // camera_pos = camera_pos + player.get_position();
+            for (auto tile : tiles) {
+                tile_ss.bind();
+                glBindVertexArray(vao);
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+                auto model_mat = glm::mat4(1.0f);
+                // tile position is from zero to one
+                model_mat = glm::translate(model_mat,
+                                           glm::vec3(-ASPECT_RATIO + (tile.get_position().x) *
+                                                     2 * ASPECT_RATIO,
+                                                     1.0 - (tile.get_position().y) * 2,
+                                                     0.0f));
+                model_mat = glm::scale(model_mat, glm::vec3(TILE_SCALE));
+                glUniformMatrix4fv(model_uniform_loc, 1, GL_FALSE, glm::value_ptr(model_mat));
+                auto view_mat = glm::mat4(1.0f);
+                camera_pos = {
+                    - (-ASPECT_RATIO + (player.get_position().x) * 2 * ASPECT_RATIO),
+                    - (1.0 - (player.get_position().y) * 2),
+                };
+                view_mat = glm::translate(view_mat, glm::vec3(camera_pos, -1.0f));
+                glUniformMatrix4fv(view_uniform_loc, 1, GL_FALSE,
+                                   glm::value_ptr(view_mat));
+                glUniformMatrix4fv(projection_uniform_loc, 1, GL_FALSE,
+                                   glm::value_ptr(projection_mat));
+                glUniform2fv(tile_ss_count_uniform_loc, 1, glm::value_ptr(tile_ss.get_tile_dimensions()));
+                glm::vec2 tile_ss_index = tile_ss.get_map()[tile.get_type()];
+                glUniform2fv(tile_ss_index_uniform_loc, 1, glm::value_ptr(tile_ss_index));
+                glm::vec2 tile_ss_padding = (tile_ss.get_tile_padding()+1.0f)/tile_ss.get_file_dimensions();
+                glUniform2fv(tile_ss_padding_uniform_loc, 1, glm::value_ptr(tile_ss_padding));
+            
+                glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+            }
+
+            for (int i = 0; i < 1; i++) {
+                player_1_ss.bind();
+                auto model_mat = glm::mat4(1.0f);
+                // tile position is from zero to one
+                model_mat = glm::translate(model_mat,
+                                           glm::vec3(-ASPECT_RATIO + (player.get_position().x) *
+                                                     2 * ASPECT_RATIO,
+                                                     1.0 - (player.get_position().y) * 2,
+                                                     0.0f));
+                model_mat = glm::scale(model_mat, glm::vec3(PLAYER_SCALE));
+                model_mat = glm::scale(model_mat, glm::vec3(PLAYER_SCALE_WIDTH, 1.0f, 1.0f));
+                glUniformMatrix4fv(model_uniform_loc, 1, GL_FALSE, glm::value_ptr(model_mat));
+                auto view_mat = glm::mat4(1.0f);
+                view_mat = glm::translate(view_mat, glm::vec3(camera_pos, -0.5f));
+                glUniformMatrix4fv(view_uniform_loc, 1, GL_FALSE,
+                                   glm::value_ptr(view_mat));
+                glUniformMatrix4fv(projection_uniform_loc, 1, GL_FALSE,
+                                   glm::value_ptr(projection_mat));
+                glUniform2fv(tile_ss_count_uniform_loc, 1, glm::value_ptr(player_1_ss.get_tile_dimensions()));
+                glm::vec2 player_ss_frame = player_1_ss.get_map()[player.get_current_frame()];
+                glUniform2fv(tile_ss_index_uniform_loc, 1, glm::value_ptr(player_ss_frame));
+                glm::vec2 player_ss_padding = (player_1_ss.get_tile_padding()+1.0f)/player_1_ss.get_file_dimensions();
+                glUniform2fv(tile_ss_padding_uniform_loc, 1, glm::value_ptr(player_ss_padding));
+            
+                glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+            }
+            glfwSwapBuffers(window);
+        }
+    }
+
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
+#endif
     return 0;
 }
